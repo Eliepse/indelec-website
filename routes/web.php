d1643af0b75e0b83675c533781f7e72909a4e2dc
Elie Meignan <contact@eliepse.fr>
2020-04-03 16:23:53 +0200

    front: add welcome boilerplate

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,6 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
 $app->get('/', function (Request $request, Response $response, $args) {
-	$response->getBody()->write(
-		"<!DOCTYPE html>\n"
-		. '<html lang="fr">'. "\n"
-		. "<head>\n"
-		. '<meta charset="UTF-8">' . "\n"
-		. "<title>" . env("META_TITLE") . "</title>\n"
-		. "<link href='" . webpack("css/styles.css", "") . "' rel='stylesheet' type='text/css' />\n"
-		. "</head>\n"
-		. "<body>\n"
-		. "\tHello world!\n"
-		. "\t<script src='" . webpack("js/index.js", "") . "'></script>\n"
-		. "</body>\n"
-		. "</html>\n");
+	$response->getBody()->write(view("welcome", ["name" => "INDéLEC"]));
 	return $response;
 });
