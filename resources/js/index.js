127ce99bb540f31b11002da088ece9bb39460888
Elie Meignan <contact@eliepse.fr>
2020-04-04 13:08:45 +0200

    front: add main navigation

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+window.addEventListener('scroll', checkScroll);
+
+function checkScroll() {
+	if (window.scrollY > 50) {
+		document.body.classList.add('scrolled');
+	} else {
+		document.body.classList.remove('scrolled');
+	}
+}
