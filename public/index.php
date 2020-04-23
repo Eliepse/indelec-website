df48341194c4703ae1906a12e3d7dae36cb4b36e
Elie Meignan <contact@eliepse.fr>
2020-04-23 11:13:51 +0200

    core: add required env values

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 $dotenv->required('APP_ENV')->notEmpty()->allowedValues(['local', 'production']);
+$dotenv->required('META_TITLE')->notEmpty();
+$dotenv->required('META_DESCRIPTION')->notEmpty();
 
 $app = AppFactory::create();
 
