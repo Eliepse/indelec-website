11800983d955821f8f5392f5d549dc08d40f24b8
Elie Meignan <contact@eliepse.fr>
2020-04-25 10:35:05 +0200

    core: add service provider

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
+declare(strict_types=1);
 
 use App\App;
 use App\Middlewares\EscapeRequestContentMiddleware;
 use App\Middlewares\JsonBodyParserMiddleware;
-use Slim\Factory\AppFactory;
+use DI\Bridge\Slim\Bridge;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -15,17 +16,18 @@ $dotenv->required('META_DESCRIPTION')->notEmpty();
 $dotenv->required('CONTACT_TARGET_MAIL')->notEmpty();
 $dotenv->required('MAIL_FROM_ADDRESS')->notEmpty();
 
-$app = AppFactory::create();
+$app = Bridge::create();
 
 App::setApp($app);
 
 
-$app->addRoutingMiddleware();
+// Add global middlewares
 $app->addMiddleware(new JsonBodyParserMiddleware());
 $app->addMiddleware(new EscapeRequestContentMiddleware());
-
+$app->addRoutingMiddleware();
 $app->addErrorMiddleware(app()->isLocal(), true, true);
 
+// Setup routes
 include_once '../routes/web.php';
 
 $app->run();
