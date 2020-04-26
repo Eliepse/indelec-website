91299a7d445656c2e295b1b5429bd57283d2a40e
Elie Meignan <contact@eliepse.fr>
2020-04-26 09:32:36 +0200

    feat: add lazy-loader

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import yall from "yall-js";
 import {checkScroll} from "./lib/scrollChecker";
 import {unfakemail} from "./lib/fakemail";
 import "./lib/caroussel";
@@ -8,3 +9,7 @@ window.addEventListener('scroll', checkScroll);
 checkScroll();
 
 document.querySelectorAll("a[fakemail]").forEach($link => unfakemail($link));
+
+document.addEventListener("DOMContentLoaded", function() {
+	yall({}); // Todo: Add lazy-loading for inline background-images
+});
