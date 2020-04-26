b78aca5ade363ca436c96ee041c40fcaea997ba3
Elie Meignan <contact@eliepse.fr>
2020-04-26 13:09:49 +0200

    core: composer install change

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git pull
-composer install -q --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
+composer install --no-dev --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
 yarn install
 yarn run prod
