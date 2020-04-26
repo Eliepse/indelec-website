90d962cdf7c8199c566d6819b47f138f6c7ee773
Elie Meignan <contact@eliepse.fr>
2020-04-26 12:35:48 +0200

    core: enable composer output

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git pull
-composer install -q --no-ansi --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
+composer install -q --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
 yarn install
 yarn run prod
