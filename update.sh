f5681a5c45814221a3a38d809f96f25873b42b19
Elie Meignan <contact@eliepse.fr>
2020-04-26 16:27:28 +0200

    core: more operational update script

--- update.sh
+++ update.sh
@@ -1,5 +1,18 @@
+echo "Pulling repository"
 git pull
+
+echo "Removing vendors (js|php)"
+rm -rf ./node_modules
+rm -rf ./vendor
+
+echo "Installing back-end packages"
 composer install --no-dev --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
+
+echo "Installing front-end packages"
 yarn install
+
+echo "Building front-end files"
 yarn run prod
+
+echo "Removing cache"
 rm -rf ./storage/cache/*
