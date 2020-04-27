ee1df8e0fde5384d76faf1da8d91a907ecdb01bb
Élie Meignan <contact@eliepse.fr>
2020-04-27 20:52:13 +0200

    core: correct composer arguments

--- update.sh
+++ update.sh
@@ -1,18 +1,26 @@
+#!/bin/sh
+
+echo ""
 echo "Pulling repository"
 git pull
 
+echo ""
 echo "Removing vendors (js|php)"
 rm -rf ./node_modules
 rm -rf ./vendor
 
+echo ""
 echo "Installing back-end packages"
-composer install --no-dev --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
+composer install --ansi --no-dev --no-interaction --profile --no-scripts --no-suggest --no-progress --prefer-dist --optimize-autoloader
 
+echo ""
 echo "Installing front-end packages"
 yarn install
 
+echo ""
 echo "Building front-end files"
 yarn run prod
 
+echo ""
 echo "Removing cache"
 rm -rf ./storage/cache/*
